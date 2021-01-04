b044725bb9d95ec2b9d7f2a807e5bafd20c3f292
ryan <ryan062692@gmail.com>
2021-01-04 06:38:31 -0500

    Delete Item Complete

--- frontend/src/components/Item.js
+++ frontend/src/components/Item.js
@@ -1,7 +1,11 @@
 import ListItem from '@material-ui/core/ListItem';
 import Typograhpy from '@material-ui/core/Typography';
 import PropTypes from 'prop-types';
+import DeleteIcon from '@material-ui/icons/DeleteForever';
+import IconButton from '@material-ui/core/IconButton';
 import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { deleteItem } from 'store/items/itemsThunks';
 
 Item.propTypes = {
   item: PropTypes.object.isRequired,
@@ -12,6 +16,7 @@ export default function Item({ item, provided, snapshot }) {
   const [isHovering, setIsHovering] = useState(false);
   const { dragHandleProps, draggableProps, innerRef } = provided;
   const { isDragging } = snapshot;
+  const dispatch = useDispatch();
   let background = 'darkgrey';
   if (isDragging) background = 'lightgreen';
   else if (isHovering) background = 'grey';
@@ -24,6 +29,7 @@ export default function Item({ item, provided, snapshot }) {
       onMouseEnter={() => setIsHovering(true)}
       onMouseLeave={() => setIsHovering(false)}
       style={{
+        display: 'flex',
         userSelect: 'none',
         padding: 8 * 2,
         margin: `0 0 ${8}px 0`,
@@ -34,6 +40,14 @@ export default function Item({ item, provided, snapshot }) {
       }}
     >
       <Typograhpy>{item.name}</Typograhpy>
+      <div style={{ flexGrow: 1 }} />
+      <IconButton
+        onClick={() => {
+          dispatch(deleteItem({ id: item.id }));
+        }}
+      >
+        <DeleteIcon />
+      </IconButton>
     </ListItem>
   );
 }
