7efcd41db3c1e999bd0e6240d49bb1644f7e23d1
ryan <ryan062692@gmail.com>
2021-01-06 06:35:23 -0500

    Do Calculations

--- frontend/src/components/selected/SelectedItem.js
+++ frontend/src/components/selected/SelectedItem.js
@@ -3,19 +3,20 @@ import Typograhpy from '@material-ui/core/Typography';
 import PropTypes from 'prop-types';
 import { useState } from 'react';
 import TextField from '@material-ui/core/TextField';
-// import { useDispatch } from 'react-redux';
+import { useDispatch } from 'react-redux';
+import { itemsActions } from 'store/items/itemsReducer';
 
-Item.propTypes = {
+SelectedItem.propTypes = {
   item: PropTypes.object.isRequired,
   provided: PropTypes.object.isRequired,
   snapshot: PropTypes.object.isRequired,
+  currentTotalTime: PropTypes.number.isRequired,
 };
-export default function Item({ item, provided, snapshot }) {
+export default function SelectedItem({ item, provided, snapshot,currentTotalTime }) {
   const [isHovering, setIsHovering] = useState(false);
-  const [totalMinutes, setTotalMinutes] = useState(item.totalMinutes)
   const { dragHandleProps, draggableProps, innerRef } = provided;
   const { isDragging } = snapshot;
-  // const dispatch = useDispatch();
+  const dispatch = useDispatch();
   let background = 'darkgrey';
   if (isDragging) background = 'lightgreen';
   else if (isHovering) background = 'white';
@@ -34,22 +35,25 @@ export default function Item({ item, provided, snapshot }) {
         margin: `0 0 ${8}px 0`,
         borderRadius: '10px',
         background,
-        height: `${Math.ceil(1*totalMinutes)}rem`,
+        height: `${Math.ceil(1 * item.totalMinutes)/2}rem`,
         ...draggableProps.style,
       }}
-
     >
-      <Typograhpy>{item.name}</Typograhpy> 
+      <Typograhpy>{item.name}</Typograhpy>
       <div style={{ flexGrow: 1 }} />
+      {`Here Is The CUrrent Calculated Time ${1}`}
       <TextField
         variant="outlined"
         placeholder="minutes"
         type="number"
-        value={totalMinutes}
+        value={item.totalMinutes}
         onChange={(e) => {
-          setTotalMinutes(parseInt(e.target.value));
+          dispatch(
+            itemsActions.updateSelectedItemTotalTimeAction({ id: item.id, totalMinutes: parseInt(e.target.value) })
+          );
         }}
       />
+  {`Total Time: ${currentTotalTime}`}
     </ListItem>
   );
 }
